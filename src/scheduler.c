09b122bdd43fd73ecd4af35b4f3ef7994ee700e5
petrzhu <32615514+petrzhu@users.noreply.github.com>
2017-11-20 09:09:23 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -5,7 +5,7 @@
  *  Author: e1400460, e1400461
  */
 #include "scheduler.h"
-#include "avr\iom64.h"
+
 struct _sched_task_t {
 	uint8_t minutes;
 	uint8_t seconds;
@@ -20,63 +20,73 @@ volatile uint8_t _sched_hours;
 
 volatile struct _sched_task_t _sched_tasks[SCHEDULER_MAX_TASKS];
 
-void scheduler_init(uint8_t minutes, uint8_t seconds) {
-	int16_t index;
+void scheduler_init(void) {
+	//int16_t index;
 	
-	TIMSK &= ~(_BV(OCIE1A));
+
+	//TIMSK &= (1 << OCIE1A);
+
+	//for(index=0; index<SCHEDULER_MAX_TASKS; index++)
+	//_sched_tasks[index].task = NULL;
 	
-	for(index=0; index<SCHEDULER_MAX_TASKS; index++)
-	_sched_tasks[index].task = NULL;
-	
-	_sched_minutes = minutes;
-	_sched_seconds = seconds;
+	_sched_minutes = 0;
+	_sched_seconds = 0;
 
 	
-	OCR1A = 24999;
+	
 	TCCR1A = 0;
 	TCNT1=0;
+	OCR1A = 24999;
 	TCCR1B |= (1 << WGM12)|(1 << CS12);
+	TIMSK |= (1 << OCIE1A);
+	
+	//TCCR1B |= ;
 
 }
 
 
 int16_t task_add(uint8_t minutes, uint8_t seconds, void (*f)()) {
-	//int16_t index;
+	int16_t index;
 	volatile struct _sched_task_t *task;
 	task = &_sched_tasks[0];
-	//for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
-		//task = &_sched_tasks[index];
-		//if (task->task == NULL) {
+	for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
+		task = &_sched_tasks[index];
+		if (task->task == NULL) {
 			TIMSK &= ~(_BV(OCIE1A));
 			task->minutes = minutes;
 			task->seconds = seconds;
 			task->task = f;
-			//TIMSK |= _BV(OCIE1A);
-			//return index;
-		//}
-	//}
+			TIMSK |= _BV(OCIE1A);
+			return index;
+		}
+	}
 	return -1;
 }
 
 ISR(TIMER1_COMPA_vect) {
 	int16_t index;
 	volatile struct _sched_task_t *task;
-
+	
 	_sched_seconds += 1;
 	if (_sched_seconds == 60) {
 		_sched_seconds = 0;
 		
-		task = &_sched_tasks[0];
-		if ((task->task != NULL)  && ( _sched_seconds % task->seconds == 0))
-		task->task();
+		//task = &_sched_tasks[0];
+		//if ( _sched_seconds  == 4)
+		//task->task();
+		
 		/*for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
 			task = &_sched_tasks[index];
 			if ((task->task != NULL)  && ( _sched_seconds % task->seconds == 0))
 			task->task();
 		}*/
-
-	}
-	
+		}
+		task = &_sched_tasks[0];
+		for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
+			task = &_sched_tasks[index];
+			if ((task->task != NULL)  && ( _sched_seconds % task->seconds == 0))
+			task->task();
+		}
 	
 	
 }
