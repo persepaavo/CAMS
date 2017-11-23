a81901bbbeecae3aee3e54c5d2f3c0f707cfb1bf
petrzhu <32615514+petrzhu@users.noreply.github.com>
2017-11-23 15:22:25 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,64 +1,43 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *	hi
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 #include <asf.h>
 #include <util/delay.h>
 #include "scheduler.h"
 
-void task1(void)
+void task1(int* stack)
 {
-	PORTB ^= 0x01;
+	*stack = (int)0x01;
+	PORTB ^= *stack;
+	
 
-}
-
-void task2(void)
-{	
-	PORTB ^= 0x02;
 	
 }
-void task3(void)
+
+void task2(int* stack)
 {
-	PORTB ^= 0x04;
+	*stack = (int)0x02;	
+	PORTB ^= *stack;
+	_delay_ms(1000);
+	if(*stack == 0x2)
+	PORTB ^= 0xf3;
+
+}
+void task3(int* stack)
+{
+	*stack = (int)0x04;
+
+	PORTB ^= *stack;
 }
 
 int main (void)
 {
-	PORTB =0xff;
-
+	PORTB = 0xff;
 	
 	DDRB ^= 0xff;
-
 	board_init();
-	task_add(0,2,task1);
-	task_add(0,4,task2);
-	task_add(0,8,task3);
+	int *stack = (int*)malloc(sizeof(int)*SCHEDULER_MAX_TASKS );
+	
+	task_add(2,task1,stack);
+	task_add(4,task2,stack);
+	task_add(2,task3,stack);
 	scheduler_init();
 	sei();
 	while(1) 
