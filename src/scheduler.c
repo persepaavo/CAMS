68fed5835257cacb71a487379a57c1c3254cbc4e
petrzhu <32615514+petrzhu@users.noreply.github.com>
2017-11-23 15:22:39 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -7,39 +7,38 @@
 #include "scheduler.h"
 
 struct _sched_task_t {
-	uint8_t minutes;
+
 	uint8_t seconds;
 	void (*task)();
-
+	int *stack;
 };
-int b = 0;
 
-volatile uint8_t _sched_minutes;
+
+
 volatile uint8_t _sched_seconds;
-volatile uint8_t _sched_hours;
+volatile uint16_t _sched_mseconds;
 
 volatile struct _sched_task_t _sched_tasks[SCHEDULER_MAX_TASKS];
 
 void scheduler_init(void) {
-	//int16_t index;
-	
-	_sched_minutes = 0;
+
+	_sched_mseconds= 0;
 	_sched_seconds = 0;
 
-	
-	
+	OCR1A= 0x0007;	
 	TCCR1A = 0;
+
 	TCNT1=0;
-	OCR1A = 24999;
-	TCCR1B |= (1 << WGM12)|(1 << CS12);
+	TCCR1B = 0;
+
+	TCCR1B |= (1<<CS12) | (1<< CS10);
+	TCCR1B |= (1 << WGM12);
 	TIMSK |= (1 << OCIE1A);
 	
-
-
 }
 
 
-int16_t task_add(uint8_t minutes, uint8_t seconds, void (*f)()) {
+int16_t task_add(uint8_t seconds, void (*f)(), int *stack) {
 	int16_t index;
 	volatile struct _sched_task_t *task;
 	task = &_sched_tasks[0];
@@ -47,9 +46,10 @@ int16_t task_add(uint8_t minutes, uint8_t seconds, void (*f)()) {
 		task = &_sched_tasks[index];
 		if (task->task == NULL) {
 			TIMSK &= ~(_BV(OCIE1A));
-			task->minutes = minutes;
 			task->seconds = seconds;
 			task->task = f;
+			task->stack = &stack[index];
+			task->stack = 0;
 			TIMSK |= _BV(OCIE1A);
 			return index;
 		}
@@ -60,30 +60,42 @@ int16_t task_add(uint8_t minutes, uint8_t seconds, void (*f)()) {
 ISR(TIMER1_COMPA_vect) {
 	int16_t index;
 	volatile struct _sched_task_t *task;
-	
-	_sched_seconds += 1;
-	if (_sched_seconds == 60) {
-		_sched_seconds = 0;
+	_sched_mseconds++;
+	if (_sched_mseconds == 1000) {
+		_sched_mseconds = 0;
 		
+		_sched_seconds++;
+		if (_sched_seconds == 60)
+		{
+			_sched_seconds = 0;
 
 		}
+
+
+	
 	
 
+	
+	for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
+		task = &_sched_tasks[index];
+		
+		if ((task->task != NULL)  && ( _sched_seconds % task->seconds == 0))
+		//if (b == 0)
+		//{
+		//		b = 1;
+		{
+			//TIMSK &= ~(_BV(OCIE1A));
 
-			
-		for(index=0; index<SCHEDULER_MAX_TASKS; index++) {
-			task = &_sched_tasks[index];
-			if ((task->task != NULL)  && ( _sched_seconds % task->seconds == 0))
-				//if (b == 0)
-				//{
-			//		b = 1;
-				{
-					//TIMSK &= ~(_BV(OCIE1A));
-					task->task();
-					//TIMSK |= _BV(OCIE1A);
-				}
-			//	}
-			//b = 0;
-					
+			task->task(task->stack);
+			task->stack;
+			//TIMSK |= _BV(OCIE1A);
 		}
+		//	}
+		//b = 0;
+		
+	}
+
+	
+	}
+			
 }
