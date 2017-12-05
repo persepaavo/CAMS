918f738db266b13c5aee09d8e4960bc33fb8ce2b
persepaavo <henri.utter@gmail.com>
2017-12-05 15:16:34 +0200

    Working vehicle counter for traffic measurement

--- src/main.c
+++ src/main.c
@@ -4,35 +4,41 @@
 #include <util/delay.h>
 
 
-int valoportti;
 
-ISR(INT5_vect)
-{
-	valoportti++;
-	printf("Autoja: %d\r\n", valoportti);
-	_delay_ms(300);
-}
+
 
 void main(){
-	
+	unsigned int vehicle = 0;
+	char blocked = 0;
 	UART1_int(9600,8,1,0);
-	EIMSK |= (1 << INT5);
-	EICRB |= (1 << ISC50);
-	EICRB |= (1 << ISC51);
+
 
 	fdevopen(UART1_Put_Char_blocking,NULL);
-	sei();
 
-	DDRA |= (1<<PINA4);
+	DDRA |= (1<<PINA4);				// PINA4 as output
+	DDRA &= ~(1<<PINA5);			// PINA5 as input
 	
-	PORTA |= (1<<PINA4);
+	PORTA |= (1<<PINA4);			// Test cycle for PINA4
 	PORTA &= ~(1<<PINA4);
-	_delay_ms(2000);
-//		printf("Testi\n\r");
-	while (1){		
-		PORTA |= (1<<PINA4);
-		_delay_us(167);
-		PORTA &= ~(1<<PINA4);
-		_delay_us(167);
+	_delay_ms(2000);				// wait 2s before doing anything just in case.
+
+	while(1){
+		PORTA |= (1<<PINA4);		// Set PINA4 HIGH
+		_delay_us(100);				// IR LED pulse width
+		PORTA &= ~(1<<PINA4);		// Set PINA4 LOW
+		
+		_delay_us(200);				// wait 200us for IR sensor to drive PINA5 down, if not blocked
+		if(PINA & 0b00100000){		// If PINA5 is high there is something blocking the IR emitter
+			blocked = 1;
+		}
+		else{						// Else IR light reaches sensor
+			if(blocked == 1){		// If IR was blocked earlier a vehicle has passed
+				vehicle++;
+				blocked = 0;
+			}
+		}
+		
+		_delay_ms(25);				// wait 25ms before starting a new IR check
+		printf("Vehicles: %d\r\n", vehicle);
 	}
 }
