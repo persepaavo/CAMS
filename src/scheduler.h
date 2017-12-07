f51a9f68ba59e5835b8e58efcfdfa7b6e0570d65
petrzhu <32615514+petrzhu@users.noreply.github.com>
2017-12-07 21:05:25 +0200

    Comments are added in a header file

--- src/scheduler.h
+++ src/scheduler.h
@@ -2,7 +2,7 @@
  * scheduler.h
  *
  * Created: 25.10.2017 9:11:38
- *  Author: e1400460
+ *  Author: e1400460, e1400461, e1400481
  */ 
 
 
@@ -19,9 +19,22 @@ extern "C" {
 	#include <avr/interrupt.h>
 	#include <stdint.h>
 
+	/**************************
+	This macro defines a maximum amount of tasks performed by AVR microcontroller
+	**************************/
 
 	#define SCHEDULER_MAX_TASKS 5
 
+	/*************************
+	Task structure:
+	start_seconds, start_minutes are used to set the time of the particular task start;
+	period_sec, period_min are the variables of a period of a task;
+	the range of minutes and seconds variables are 0-59; 
+	period_sec and period_min could be set to 61 in order to
+	make a task work in a one shot mode each hour;
+	
+	(*task)() is a pointer to a task function;
+	*************************/
 	struct _sched_task_t {
 
 		uint8_t start_seconds;
@@ -30,10 +43,18 @@ extern "C" {
 		uint8_t period_min;
 		void (*task)();
 		};
-
+	
+	/************************
+	This function initializes the scheduler
+	************************/
+	
 	void scheduler_init(void);
 
 
+	/************************
+	This function adds the tasks to the _sched_task_t array;
+	All the variables are related to the variables of a _sched_task_t structure
+	************************/
 	int16_t task_add(uint8_t period_seconds, uint8_t period_minutes, 
 					uint8_t start_time_seconds, uint8_t start_time_minutes, 
 					void (*f)());
