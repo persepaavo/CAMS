e4baa0fce98ed7f36ddb07a526b4bb5be9674914
petrzhu <32615514+petrzhu@users.noreply.github.com>
2017-12-07 21:09:54 +0200

    testing file

--- src/main.c
+++ src/main.c
@@ -1,30 +1,23 @@
 #include <asf.h>
-#include <util/delay.h>
 #include "scheduler.h"
 
-void task1(int* stack)
+void task1()
 {
-	*stack = (int)0x01;
-	PORTB ^= *stack;
-	
 
-	
+	PORTB ^= 0x01;	
 }
 
-void task2(int* stack)
+void task2()
 {
-	*stack = (int)0x02;	
-	PORTB ^= *stack;
-	_delay_ms(1000);
-	if(*stack == 0x2)
-	PORTB ^= 0xf3;
+
+	PORTB ^= 0x02;
 
 }
-void task3(int* stack)
+void task3()
 {
-	*stack = (int)0x04;
 
-	PORTB ^= *stack;
+
+	PORTB ^= 0x04;
 }
 
 int main (void)
@@ -33,11 +26,11 @@ int main (void)
 	
 	DDRB ^= 0xff;
 	board_init();
-	int *stack = (int*)malloc(sizeof(int)*SCHEDULER_MAX_TASKS );
+
 	
-	task_add(2,task1,stack);
-	task_add(4,task2,stack);
-	task_add(2,task3,stack);
+	task_add(2,0, 0, 0,task1);
+	task_add(4,0,6,0,task2);
+	task_add(61, 61, 3, 0,task3);
 	scheduler_init();
 	sei();
 	while(1) 
