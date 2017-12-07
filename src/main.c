248d2aa7fe9288db0468f93803377a493b010518
petrzhu <32615514+petrzhu@users.noreply.github.com>
2017-12-07 14:40:32 +0200

    Task scheduler final version

--- src/main.c
+++ src/main.c
@@ -1,58 +1,43 @@
-#include "uart.h"
-#include "SHT11.h"
-#include <stdio.h>
+#include <asf.h>
 #include <util/delay.h>
+#include "scheduler.h"
+
+void task1()
+{
+
+	PORTB ^= 0x01;	
+}
+
+void task2()
+{
+
+	PORTB ^= 0x02;
+
+}
+void task3()
+{
 
 
-void main(){
-	
-	
-	signed char t;
-	signed char c;
-	
-	UART1_int(9600,8,1,0);
-	
-	fdevopen(UART1_Put_Char_blocking,NULL);
-//	printf("Testi\n\r");
-	_delay_ms(2000);
-	while (1){
-		change_resolution(0);
-/*
-		send_start();
-		t = send_byte(MeasureHumi);
-	
-		for(t=0; t < 20; t++){		// Wait for measurement to be ready (timeout at 20ms)
-			_delay_ms(1);
-			if((DataRead) == 0){
-				break;
-			}
-		}
-				
-		c = (unsigned char)read_byte(1);
-*/	
+	PORTB ^= 0x04;
+}
 
-
-		int16_t temp_c;
-		temp_c = get_temp();
-		
-		c = (signed char)(temp_c % 10);
-		if(c < 0){
-			c=c * -1;
-		}
-		t = (signed char)(temp_c / 10);
-		
-		printf("Temp: %d.%dC", t,c);
-		
-
-		int16_t humi_lin;
-		humi_lin = get_humi(c, t);
-		
-		c = (signed char)(humi_lin % 10);
-
-		t = (signed char)(humi_lin / 10);
-				
-		printf("  Humidity: %d.%d%%\n\r", t,c);
+int main (void)
+{
+	PORTB = 0xff;
 	
-		_delay_ms(2000);
-	}	
+	DDRB ^= 0xff;
+	board_init();
+
+	
+	task_add(2,0, 0, 0,task1);
+	task_add(4,0,6,0,task2);
+	task_add(61, 61, 3, 0,task3);
+	scheduler_init();
+	sei();
+	while(1) 
+	{
+
+	}
+
+	return 0;
 }
--- src/scheduler.h
+++ src/scheduler.h
@@ -2,7 +2,7 @@
  * scheduler.h
  *
  * Created: 25.10.2017 9:11:38
- *  Author: e1401162
+ *  Author: e1400460
  */ 
 
 
@@ -10,7 +10,37 @@
 #define SCHEDULER_H_
 
 
+#ifdef __cplusplus
+extern "C" {
+	#endif
 
+	#include <avr/io.h>
+	#include <stddef.h>
+	#include <avr/interrupt.h>
+	#include <stdint.h>
+
+
+	#define SCHEDULER_MAX_TASKS 5
+
+	struct _sched_task_t {
+
+		uint8_t start_seconds;
+		uint8_t start_minutes;
+		uint8_t period_sec;
+		uint8_t period_min;
+		void (*task)();
+		};
+
+	void scheduler_init(void);
+
+
+	int16_t task_add(uint8_t period_seconds, uint8_t period_minutes, 
+					uint8_t start_time_seconds, uint8_t start_time_minutes, 
+					void (*f)());
+
+	#ifdef __cplusplus
+}
+#endif
 
 
 #endif /* SCHEDULER_H_ */
